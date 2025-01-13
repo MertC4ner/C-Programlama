827a91cc70c89b6e63ae46f790d53467c0f3c6ed
Mert Caner Gedikli <mertcanergdkl@gmail.com>
2025-01-13 16:32:05 +0300

    Add files via upload

---
+++ #04.1 Hızlı Değer Atma ve Değer Arttırımı.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int i;
+	i = 0;
+	printf("%d\n", i);
+	printf("%d\n", i++); // once mevcut deger yazilacak, sonra deger arttirilacak
+	printf("%d\n", i);
+	printf("%d\n", ++i); // once deger arttirilacak, sonra yazdirma islemi yapilacak
+	printf("%d\n", i);
+	
+
+	return 0;
+	
+}
