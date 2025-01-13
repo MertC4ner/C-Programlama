27fc1a648dad91e056d54961e9acea9fa1603e35
Mert Caner Gedikli <mertcanergdkl@gmail.com>
2025-01-13 16:32:36 +0300

    Add files via upload

---
+++ #05.1 Typecasting Sayı Sistemleri Birim Değişimleri.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 35; // 10'luk sayi duzeninde (decimal)
+	int b = 0b00100011; // 2'lik sayi duzeninde (binary)
+	int c = 0x23; // 16'lik sayi duzeninde (hexadecimal)
+	
+	printf("a = %d\n", a);
+	printf("b = %d\n", b);
+	printf("c = %d\n", c);
+	
+	return 0;
+}
