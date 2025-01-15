0950485495e9db50b1666ee646fbf283b726a746
Mert Caner Gedikli <mertcanergdkl@gmail.com>
2025-01-15 23:33:36 +0300

    Add files via upload

---
+++ #20.1 Merdiven İf Else Yapıları.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+	int sayi1, sayi2, sayi3, maksimum;
+	
+	// Kullanicidan 3 sayi degeri al
+	printf("Lutfen 3 adet sayi giriniz: ");
+	scanf("%d%d%d", &sayi1, &sayi2, &sayi3);
+	
+	if((sayi1 > sayi2) && (sayi1 > sayi3))
+	{
+		// sayi1 > sayi2 ve sayi1 > sayi3 ise
+		maksimum = sayi1;
+	}
+	else if(sayi2 > sayi3)
+	{
+		// sayi1 >sayi2 degilse ve sayi2 > sayi3 ise
+		maksimum = sayi2;
+	}
+	else
+	{
+		// sayi1 > sayi2 degilse, ayrica sayi2 > sayi3 degilse
+		maksimum = sayi3;
+	}
+	
+	// Maksimum degeri yazdir
+	printf("3 sayi icerisinde en buyuk deger = %d", maksimum);
+	
+	return 0;
+}
