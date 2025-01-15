d548c4fce1d85fc348dd8926953c699b7165a2dc
Mert Caner Gedikli <mertcanergdkl@gmail.com>
2025-01-16 00:26:39 +0300

    Add files via upload

---
+++ #21.1 İf Else İle Matematiksel Problemler.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+	int yil;
+	
+	// Kullanicidan yil degerini aliniz
+	printf("Yil degerini giriniz: ");
+	scanf("%d", &yil);
+	
+	/*
+	 * Deger 4'e bolunebiliyorsa ve 100'e bolunemiyorsa
+	 * veya deger 400'e bolunebiliyorsa
+	 * ilgili yil Artik yildir.
+	 * Aksi takdirde Artik yil degildir.
+	 */
+	 
+	 if(((yil % 4 == 0) && (yil % 100 != 0)) || (yil % 400 == 0))
+	 {
+	 	printf("Soz konusu yil, artik yildir...");
+	 }
+	 else
+	 {
+	 	printf("Soz konusu yil, artik yil degildir...");
+	 }
+	 
+	 return 0;
+
+	
+}
