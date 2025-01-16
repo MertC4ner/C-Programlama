217d26ebf3d2caf524b18c02e518563842c11255
Mert Caner Gedikli <mertcanergdkl@gmail.com>
2025-01-16 14:29:31 +0300

    Add files via upload

---
+++ #22.1 Alfabetik Karakterler Switch Case.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main()
+{
+	int gun;
+	
+	// Kullanicidan, haftanin kacinci gunu oldugunun bilgisini al
+	printf("Gunun, haftanin kacinci gunu oldugunu giriniz (1-7): ");
+	scanf("%d", &gun);
+	
+	if(gun == 1)
+	{
+		printf("Pazartesi");
+	}
+	else if(gun == 2)
+	{
+		printf("Sali");
+	}
+	else if(gun == 3)
+	{
+		printf("Carsamba");
+	}
+	else if(gun == 4)
+	{
+		printf("Persembe");
+	}
+	else if(gun == 5)
+	{
+		printf("Cuma");
+	}
+	else if(gun == 6)
+	{
+		printf("Cumartesi");
+	}
+	else if(gun == 7)
+	{
+		printf("Pazar");
+	}
+	else
+	{
+		printf("Gecersiz deger!... Lutfen 1 ile 7 arasinda bir sayi giriniz");
+	}
+	
+	return 0;
+}
