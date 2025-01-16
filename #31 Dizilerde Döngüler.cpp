f343a5e8c78d42814378124f8e2546a2b8187e58
Mert Caner Gedikli <mertcanergdkl@gmail.com>
2025-01-17 01:32:17 +0300

    Add files via upload

---
+++ #31 Dizilerde Döngüler.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int benimDizim[10] = {25, 50, 75, 100};
+	// benimDizim[0] = 33;
+	int i;
+	for(i = 0; i < 10; i++)
+	{
+		printf("%d ", benimDizim[i]);
+	}
+	
+	return 0;
+	
+}
